f39fb2e71bb6f70e2d6051bab539bfebabbc24e8
Haidar <muhammad_rijaldi_25rpl@student.smktelkom-mlg.sch.id>
2018-04-28 00:40:09 +0700

    Finishing

--- app/src/main/java/com/example/haidar/testmagang/CommentActivity.java
+++ app/src/main/java/com/example/haidar/testmagang/CommentActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -44,12 +43,10 @@ public class CommentActivity extends AppCompatActivity {
         getDataPost();
 
         TextView tvTitle = findViewById(R.id.textViewDetailName);
-        tvTitle.setText(pModel.getTitle());
+        tvTitle.setText("Title : " + pModel.getTitle());
         TextView tvBody = findViewById(R.id.textViewDetailPost);
-        tvBody.setText(pModel.getBody());
+        tvBody.setText("Body  : " + pModel.getBody());
 
-        //url = "http://jsonplaceholder.typicode.com/posts/" + pModel.getId() + "/comments";
-        Log.d("comment", "onCreate: " + url);
     }
 
     public void getDataPost() {
